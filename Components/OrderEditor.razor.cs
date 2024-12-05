20f9b18d5c7801aac3b5b4669083ec5e1f1f408d
DataJuggler <corby@datajuggler.com>
2024-12-05 10:52:54 -0600

    I am working on the OrderEditor

---
+++ Components/OrderEditor.razor.cs
@@ -0,0 +1,123 @@
+﻿
+
+#region using statements
+
+using DataJuggler.Blazor.Components;
+using DataJuggler.Blazor.Components.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+#endregion
+
+namespace AIPizza.Components
+{
+
+    #region class OrderEditor
+    /// <summary>
+    /// This class is used to create and edit orders
+    /// </summary>
+    public partial class OrderEditor : IBlazorComponent, IBlazorComponentParent
+    {
+        
+        #region Private Variables
+        private List<IBlazorComponent> children;
+        private string name;
+        private IBlazorComponentParent parent;
+        #endregion
+        
+        #region Methods
+            
+            #region ReceiveData(Message message)
+            /// <summary>
+            /// method Receive Data
+            /// </summary>
+            public void ReceiveData(Message message)
+            {
+                
+            }
+            #endregion
+            
+            #region Refresh()
+            /// <summary>
+            /// method Refreshes the UI
+            /// </summary>
+            public void Refresh()
+            {
+                InvokeAsync(() =>
+                {
+                    StateHasChanged();
+                });
+            }
+            #endregion
+            
+            #region Register(IBlazorComponent component)
+            /// <summary>
+            /// method Register
+            /// </summary>
+            public void Register(IBlazorComponent component)
+            {
+                
+            }
+            #endregion
+            
+        #endregion
+        
+        #region Properties
+            
+            #region Children
+            /// <summary>
+            /// This property gets or sets the value for 'Children'.
+            /// </summary>
+            public List<IBlazorComponent> Children
+            {
+                get { return children; }
+                set { children = value; }
+            }
+            #endregion
+            
+            #region HasChildren
+            /// <summary>
+            /// This property returns true if this object has a 'Children'.
+            /// </summary>
+            public bool HasChildren
+            {
+                get
+                {
+                    // initial value
+                    bool hasChildren = (this.Children != null);
+                    
+                    // return value
+                    return hasChildren;
+                }
+            }
+            #endregion
+
+            #region Name
+            /// <summary>
+            /// This property gets or sets the value for 'Name'.
+            /// </summary>
+            [Parameter]
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            #endregion
+            
+            #region Parent
+            /// <summary>
+            /// This property gets or sets the value for 'Parent'.
+            /// </summary>
+            [Parameter]
+            public IBlazorComponentParent Parent
+            {
+                get { return parent; }
+                set { parent = value; }
+            }
+            #endregion
+            
+        #endregion
+
+    }
+    #endregion
+
+}
